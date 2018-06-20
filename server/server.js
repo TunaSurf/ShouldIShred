0c354e4313923a8950df41d42cb35ea8093e99c5
Chase Burgess <cbb5104@gmail.com>
2018-06-20 10:55:07 -0400

    require node-fetch and try to get buoy-js module to require

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 const express     = require('express'),
       bodyParser  = require('body-parser');
       mongoose    = require('mongoose'),
-      url = 'mongodb://localhost/shred_db',
+      fetch       = require('node-fetch'),
+      buoy        = require('buoy-js'),
+      url         = 'mongodb://localhost/shred_db',
       Location    = require('./models/location.js');
+
+// import Buoy from 'buoy-js';
+// import Tide from 'buoy-js';
 
 const app         = express();
 
@@ -18,7 +23,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
+function setFromApi(buoysURL) {
+  this.callApi(buoysURL)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+}
+
+callApi = async (buoys) => {
+  const response = await fetch(buoys);
+  const body = await response.text();
+
+  if (response.status !== 200) throw Error(body.message);
+  return body;
+}
+
 app.get('/:location', function (req, res) {
+  const buoysURL = 'http://www.ndbc.noaa.gov/data/latest_obs/latest_obs.txt';
+  
+  setFromApi(buoysURL);
+
+  
+
+
   Location.find({"key": req.params.location}, function(err, location) {
     if(err) {
       console.log(err);
@@ -28,6 +54,10 @@ app.get('/:location', function (req, res) {
     }
   });
 });
+
+// app.update('/:location', function(req, res) {
+
+// })
 
 const PORT = process.env.PORT || 5000;
 app.listen(5000, () => {
